07591fdc9e2c57abe0906a46aff36fa6c4f3ce14
Joao Paulo Euko <joaopeuko@gmail.com>
2020-11-10 13:08:13 -0300

    Version 0.1.1
    Added server-side Socket communication to integrate MQL5 Indicator as well as a simple example of an expert advisor using it.
    
    To be able to use it you need the MQL5 Service to send the data.
    
    Support to indicators:
       - Bollinger Bands
       - MACD
       - Moving Average
       - OBV On Balance Volume
       - Stochastic

---
+++ ExpertAdvisor/example_sockets_connection.py
@@ -0,0 +1,51 @@
+from Include.trade import Trade
+from Include.tick import Tick
+from Include.rates import Rates
+from Include.indicator_connector import Indicator
+
+indicator = Indicator()
+
+trade = Trade('Example',  # Expert name
+              0.1,  # Expert Version
+              'PETR4',  # symbol
+              567,  # Magic number
+              100.0,  # lot
+              10,  # stop loss - 10 cents
+              30,  # emergency stop loss - 30 cents
+              10,  # take profit - 10 cents
+              30,  # emergency take profit - 30 cents
+              '9:15',  # It is allowed to trade after that hour. Do not use zeros, like: 09
+              '17:30',  # It is not allowed to trade after that hour but let open all the position already opened.
+              '17:50',  # It closes all the position opened. Do not use zeros, like: 09
+              0.5,  # average fee
+              )
+
+time = 0
+while True:
+
+    stochastic = indicator.stochastic(symbol=trade.symbol)
+
+    tick = Tick(trade.symbol)
+    rates = Rates(trade.symbol, 1, 0, 1)
+
+    try:
+        k = stochastic['k_result']
+        d = stochastic['d_result']
+
+        if tick.time_msc != time:
+            buy = (k > d)
+            sell = (k < d)
+
+            trade.open_position(buy, sell, 'Example Advisor')
+
+    except TypeError:
+        pass
+
+    time = tick.time_msc
+
+    if trade.days_end():
+        trade.close_position('End of the trading day reached.')
+        break
+
+print('Finishing the program.')
+print('Program finished.')
---
+++ Include/indicator_connector.py
@@ -0,0 +1,240 @@
+import socket
+import json
+
+
+# To be able to use it you need the MQL5 Service to send the data, it is possible to found it here:
+# URL
+
+class Indicator:
+    def __init__(self,
+                 address='localhost',
+                 port=9090,
+                 listen=1):
+
+        self.address = address
+        self.port = port
+        self.listen = listen
+        self.location = (address, port)
+
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.bind((self.address, self.port))
+        self.s.listen(self.listen)
+
+    # -------------------------------------------------------------------- #
+    def bollinger_bands(self,
+                        symbol,
+                        time_frame=1,
+                        period=20,
+                        ma_shift=0,
+                        deviation=2.000,
+                        # applied_price:
+                        # 0 - PRICE_CLOSE
+                        # 1 - PRICE_OPEN
+                        # 2 - PRICE_HIGH
+                        # 3 - PRICE_LOW
+                        # 4 - PRICE_MEDIAN
+                        # 5 - PRICE_TYPICAL
+                        # 6 - PRICE_WEIGHTED
+                        applied_price=0):
+
+        try:
+            client_socket, address = self.s.accept()
+            message = (f"bollinger_bands,"
+                       f"{symbol},"
+                       f"{time_frame},"
+                       f"{period},"
+                       f"{ma_shift},"
+                       f"{deviation},"
+                       f"{applied_price}")
+
+            client_socket.send(bytes(message, 'utf-8'))
+            data = client_socket.recv(1024)
+
+            result = data.decode('utf-8')
+            try:
+                return json.loads(result)
+
+            except ValueError:
+                print('Connection lost to MQL5 Service')
+                pass
+
+        except ConnectionResetError:
+            pass
+
+        except ConnectionAbortedError:
+            pass
+
+    # -------------------------------------------------------------------- #
+    def macd(self,
+             symbol,
+             time_frame=1,
+             fast_ema_period=12,
+             slow_ema_period=26,
+             signal_period=9,
+             # applied_price:
+             # 0 - PRICE_CLOSE
+             # 1 - PRICE_OPEN
+             # 2 - PRICE_HIGH
+             # 3 - PRICE_LOW
+             # 4 - PRICE_MEDIAN
+             # 5 - PRICE_TYPICAL
+             # 6 - PRICE_WEIGHTED
+             applied_price=0):
+
+        try:
+            client_socket, address = self.s.accept()
+            message = (f"macd,"
+                       f"{symbol},"
+                       f"{time_frame},"
+                       f"{fast_ema_period},"
+                       f"{slow_ema_period},"
+                       f"{signal_period},"
+                       f"{applied_price}")
+
+            client_socket.send(bytes(message, 'utf-8'))
+            data = client_socket.recv(1024)
+
+            result = data.decode('utf-8')
+            try:
+                return json.loads(result)
+
+            except ValueError:
+                print('Connection lost to MQL5 Service')
+                pass
+
+        except ConnectionResetError:
+            pass
+
+        except ConnectionAbortedError:
+            pass
+
+    # -------------------------------------------------------------------- #
+    def moving_average(self,
+                       symbol,
+                       time_frame=1,
+                       period=20,
+                       start_position=0,  # Change it if you want past values, zero is the most recent.
+                       # method:
+                       # 0 - MODE_SMA
+                       # 1 - MODE_EMA
+                       # 2 - MODE_SMMA
+                       # 3 - MODE_LWMA
+                       method=0,
+                       # applied_price:
+                       # 0 - PRICE_CLOSE
+                       # 1 - PRICE_OPEN
+                       # 2 - PRICE_HIGH
+                       # 3 - PRICE_LOW
+                       # 4 - PRICE_MEDIAN
+                       # 5 - PRICE_TYPICAL
+                       # 6 - PRICE_WEIGHTED
+                       applied_price=0):
+
+        try:
+            client_socket, address = self.s.accept()
+            message = (f"moving_average,"
+                       f"{symbol},"
+                       f"{time_frame},"
+                       f"{period},"
+                       f"{start_position},"  # Change it if you want past values, zero is the most recent.
+                       f"{method},"
+                       f"{applied_price}")
+
+            client_socket.send(bytes(message, 'utf-8'))
+            data = client_socket.recv(1024)
+
+            result = data.decode('utf-8')
+            try:
+                return json.loads(result)
+
+            except ValueError:
+                print('Connection lost to MQL5 Service')
+                pass
+
+        except ConnectionResetError:
+            pass
+
+        except ConnectionAbortedError:
+            pass
+
+        # -------------------------------------------------------------------- #
+
+    def obv(self,
+            symbol,
+            time_frame=1,
+            # applied_volume:
+            # 0 - VOLUME_TICK
+            # 1 - VOLUME_REAL
+            applied_volume=0):
+
+        try:
+            client_socket, address = self.s.accept()
+            message = (f"obv,"
+                       f"{symbol},"
+                       f"{time_frame},"
+                       f"{applied_volume}")
+
+            client_socket.send(bytes(message, 'utf-8'))
+            data = client_socket.recv(1024)
+
+            result = data.decode('utf-8')
+            try:
+                return json.loads(result)
+
+            except ValueError:
+                print('Connection lost to MQL5 Service')
+                pass
+
+        except ConnectionResetError:
+            pass
+
+        except ConnectionAbortedError:
+            pass
+
+    # -------------------------------------------------------------------- #
+    def stochastic(self,
+                   symbol,
+                   time_frame=1,
+                   k_period=5,
+                   d_period=3,
+                   slowing=3,
+                   start_position=0,  # Change it if you want past values, zero is the most recent.
+                   # method:
+                   # 0 - MODE_SMA
+                   # 1 - MODE_EMA
+                   # 2 - MODE_SMMA
+                   # 3 - MODE_LWMA
+                   method=0,
+                   # applied_price
+                   # 0 - STO_LOWHIGH
+                   # 1 - STO_CLOSECLOSE
+                   applied_price=0):
+
+        try:
+            client_socket, address = self.s.accept()
+            message = (f"stochastic,"
+                       f"{symbol},"
+                       f"{time_frame},"
+                       f"{k_period},"
+                       f"{d_period},"
+                       f"{slowing},"
+                       f"{start_position},"  # Change it if you want past values, zero is the most recent.
+                       f"{method},"
+                       f"{applied_price}")
+
+            client_socket.send(bytes(message, 'utf-8'))
+            data = client_socket.recv(1024)
+
+            result = data.decode('utf-8')
+            try:
+                return json.loads(result)
+
+            except ValueError:
+                print('Connection lost to MQL5 Service')
+                pass
+
+        except ConnectionResetError:
+            pass
+
+        except ConnectionAbortedError:
+            pass
