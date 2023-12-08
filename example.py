97b8c891af27a029ec6ec847730dcb6e7d1bc3cf
Joao Paulo Euko <joaopeuko@gmail.com>
2023-12-08 12:27:37 +0000

    update

--- example.py
+++ example.py
@@ -1,40 +1,39 @@
-from include.trade import Trade
-from include.tick import Tick
-from include.rates import Rates
+from mqpy.src.rates import Rates
+from mqpy.src.tick import Tick
+from mqpy.src.trade import Trade
 
-
-trade = Trade('Example',  # Expert name
-              0.1,  # Expert Version
-              'EURUSD',  # symbol
-              567,  # Magic number
-              1.0,  # lot, it is a floating point.
-              25,  # stop loss
-              300,  # emergency stop loss
-              25,  # take profit
-              300,  # emergency take profit
-              '9:15',  # It is allowed to trade after that hour. Do not use zeros, like: 09
-              '17:30',  # It is not allowed to trade after that hour but let open all the position already opened.
-              '17:50',  # It closes all the position opened. Do not use zeros, like: 09
-              0.5,  # average fee
-              )
+trade = Trade(
+    "Example",  # Expert name
+    0.1,  # Expert Version
+    "EURUSD",  # symbol
+    567,  # Magic number
+    1.0,  # lot, it is a floating point.
+    25,  # stop loss
+    300,  # emergency stop loss
+    25,  # take profit
+    300,  # emergency take profit
+    "9:15",  # It is allowed to trade after that hour. Do not use zeros, like: 09
+    "17:30",  # It is not allowed to trade after that hour but let open all the position already opened.
+    "17:50",  # It closes all the position opened. Do not use zeros, like: 09
+    0.5,  # average fee
+)
 
 time = 0
 while True:
-
     tick = Tick(trade.symbol)
     rates = Rates(trade.symbol, 1, 0, 1)
 
     if tick.time_msc != time:
-        buy = (tick.last > rates.open)
-        sell = (tick.last < rates.open)
+        buy = tick.last > rates.open
+        sell = tick.last < rates.open
 
-        trade.open_position(buy, sell, 'Example Advisor')
+        trade.open_position(buy, sell, "Example Advisor")
 
     time = tick.time_msc
 
     if trade.days_end():
-        trade.close_position('End of the trading day reached.')
+        trade.close_position("End of the trading day reached.")
         break
 
-print('Finishing the program.')
-print('Program finished.')
+print("Finishing the program.")
+print("Program finished.")
--- mqpy/src/rates.py
+++ mqpy/src/rates.py
@@ -1,7 +1,6 @@
 from typing import List, Union
 
 import MetaTrader5 as Mt5
-from logger import log
 
 
 class Rates:
@@ -22,7 +21,6 @@ class Rates:
         """
         self._symbol = symbol
         try:
-            log.info(f"Creating Rates object for symbol: {self._symbol}")
             rates_data = Mt5.copy_rates_from_pos(self._symbol, time_frame, start_pos, period)
 
             self._time = rates_data["time"]
@@ -34,55 +32,49 @@ class Rates:
             self._spread = rates_data["spread"]
             self._real_volume = rates_data["real_volume"]
 
-            log.info(f"Rates object created for symbol: {symbol}")
+            # Optionally, you can print statements here for debugging
+            # print(f"Rates object created for symbol: {self._symbol}")
         except Exception as e:
-            log.error(f"Failed to create Rates object for symbol {symbol}. Error: {e}")
+            # Optionally, you can print statements here for debugging
+            # print(f"Failed to create Rates object for symbol {self._symbol}. Error: {e}")
             raise
 
     @property
     def time(self) -> List[Union[int, float]]:
         """List of timestamps."""
-        log.info(f"Getting time property for symbol: {self._symbol}")
         return self._time
 
     @property
     def open(self) -> List[float]:
         """List of open prices."""
-        log.info(f"Getting open property for symbol: {self._symbol}")
         return self._open
 
     @property
     def high(self) -> List[float]:
         """List of high prices."""
-        log.info(f"Getting high property for symbol: {self._symbol}")
         return self._high
 
     @property
     def low(self) -> List[float]:
         """List of low prices."""
-        log.info(f"Getting low property for symbol: {self._symbol}")
         return self._low
 
     @property
     def close(self) -> List[float]:
         """List of close prices."""
-        log.info(f"Getting close property for symbol: {self._symbol}")
         return self._close
 
     @property
     def tick_volume(self) -> List[int]:
         """List of tick volumes."""
-        log.info(f"Getting tick_volume property for symbol: {self._symbol}")
         return self._tick_volume
 
     @property
     def spread(self) -> List[int]:
         """List of spreads."""
-        log.info(f"Getting spread property for symbol: {self._symbol}")
         return self._spread
 
     @property
     def real_volume(self) -> List[int]:
         """List of real volumes."""
-        log.info(f"Getting real_volume property for symbol: {self._symbol}")
         return self._real_volume
