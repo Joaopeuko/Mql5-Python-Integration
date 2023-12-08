2d1bd57085cea95fa91a9e6f463c9fc52222dbda
Joao Paulo Euko <joaopeuko@gmail.com>
2023-12-08 12:32:41 +0000

    update

--- mqpy/src/utilities.py
+++ mqpy/src/utilities.py
@@ -4,17 +4,28 @@ import MetaTrader5 as Mt5
 
 
 class Utilities:
-    def __init__(self):
-        # Variables for minutes_counter
-        self.__minutes_counter = 0
-        self.__counter_flag = True
-        self.__allowed_to_trade = True
-        self.__allow_to_count = False
-        self.__recent_trade = False
+    """A utility class for handling trading-related functionalities."""
 
-    # This function will count the amount of minutes after some trade end.
-    # You can use it as a condition to allow trades to happen.
-    def minutes_counter_after_trade(self, symbol, count_until):
+    def __init__(self) -> None:
+        """Initialize the Utilities class."""
+        # Variables for minutes_counter
+        self.__minutes_counter: int = 0
+        self.__counter_flag: bool = True
+        self.__allowed_to_trade: bool = True
+        self.__allow_to_count: bool = False
+        self.__recent_trade: bool = False
+
+    def check_trade_availability(self, symbol: str, count_until: int) -> bool:
+        """
+        Check if trading is allowed based on specified conditions.
+
+        Args:
+            symbol (str): The financial instrument symbol.
+            count_until (int): The number of minutes until trading is allowed.
+
+        Returns:
+            bool: True if trading is allowed, False otherwise.
+        """
         if len(Mt5.positions_get(symbol=symbol)) == 1:
             self.__recent_trade = True
 
@@ -25,7 +36,7 @@ class Utilities:
                 self.__recent_trade = False
 
         if datetime.now().second == 0 and self.__counter_flag and self.__allow_to_count:
-            print(f"Your Expert Advisor will be allowed to trade in {count_until-self.__minutes_counter} minutes.")
+            print(f"Trading will be allowed in {count_until - self.__minutes_counter} minutes.")
             self.__minutes_counter += 1
             self.__counter_flag = False
 
@@ -33,10 +44,14 @@ class Utilities:
             self.__counter_flag = True
 
         if self.__minutes_counter == count_until:
-            print(f"Your Expert Advisor is allowed to trade.\n")
-            self.__minutes_counter = 0
-            self.__counter_flag = True
-            self.__allow_to_count = False
-            self.__allowed_to_trade = True
+            print("Trading is allowed.\n")
+            self.__reset_counters()
 
         return self.__allowed_to_trade
+
+    def __reset_counters(self) -> None:
+        """Reset counters after trading is allowed."""
+        self.__minutes_counter = 0
+        self.__counter_flag = True
+        self.__allow_to_count = False
+        self.__allowed_to_trade = True
