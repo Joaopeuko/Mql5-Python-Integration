b9a1343d1e8d49517b600c506701cae9f61b41bb
Joao Paulo Euko <joaopeuko@gmail.com>
2023-12-08 16:31:11 +0000

    update

--- setup.py
+++ setup.py
@@ -6,30 +6,6 @@ import setuptools
 
 long_description = (pathlib.Path(__file__).parent / "README.md").read_text()
 
-
-def get_requirements_from_pipfile_lock(pipfile_lock=None):
-    if pipfile_lock is None:
-        pipfile_lock = os.path.join(os.path.dirname(os.path.realpath(__file__)), "Pipfile.lock")
-    lock_data = json.load(open(pipfile_lock))
-    retval = []
-    for package_name, package_data in lock_data.get("default", {}).items():
-        if package_data.get("file"):
-            package_ref = f'{package_name} @ {package_data["file"]}'
-        else:
-            package_ref = f'{package_name}{package_data["version"]}'
-        retval.append(package_ref)
-    return retval
-
-
-def generate_requirements_txt(pipfile_lock_requirements):
-    for requirement in pipfile_lock_requirements:
-        with open(os.path.join("requirements.txt"), "w") as file:
-            file.writelines(requirement)
-
-
-requirements = get_requirements_from_pipfile_lock()
-generate_requirements_txt(requirements)
-
 setuptools.setup(
     name="mqpy",
     version="v0.6.0",
