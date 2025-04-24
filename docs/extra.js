929e135fc23f67dde68d20c74d2d30e51537cf3f
Joao Paulo Euko <joaopeuko@gmail.com>
2025-04-24 12:47:14 +0000

    save

--- docs/extra.js
+++ docs/extra.js
@@ -44,10 +44,71 @@ function cleanupClipboardText(targetSelector) {
     });
   }
 
+  // Show sponsor popup on first visit
+  function showSponsorPopup() {
+    // Check if user has seen the popup before
+    if (!localStorage.getItem('mqpy_sponsor_popup_shown')) {
+      // Create popup container
+      const popup = document.createElement('div');
+      popup.className = 'sponsor-popup';
+      
+      // Create popup content
+      popup.innerHTML = `
+        <div class="sponsor-popup-content">
+          <div class="sponsor-popup-header">
+            <h2>Support MQPy Development</h2>
+            <button class="sponsor-popup-close">&times;</button>
+          </div>
+          <div class="sponsor-popup-body">
+            <p>Thank you for using MQPy! This project is free and open-source, developed in my free time.</p>
+            <p>If you find MQPy useful, please consider supporting its development:</p>
+            <div class="sponsor-popup-buttons">
+              <a href="https://github.com/sponsors/Joaopeuko" class="sponsor-popup-button primary" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" fill="currentColor"/></svg>
+                Become a Sponsor
+              </a>
+            </div>
+            <div class="sponsor-popup-footer">
+              <label>
+                <input type="checkbox" id="sponsor-popup-dont-show"> Don't show this message again
+              </label>
+            </div>
+          </div>
+        </div>
+      `;
+      
+      // Add popup to body
+      document.body.appendChild(popup);
+      
+      // Show popup with animation
+      setTimeout(() => {
+        popup.classList.add('active');
+      }, 1000);
+      
+      // Close button event
+      const closeBtn = popup.querySelector('.sponsor-popup-close');
+      closeBtn.addEventListener('click', () => {
+        popup.classList.remove('active');
+        
+        // Check if "don't show again" is checked
+        const dontShowAgain = document.getElementById('sponsor-popup-dont-show').checked;
+        if (dontShowAgain) {
+          localStorage.setItem('mqpy_sponsor_popup_shown', 'true');
+        }
+        
+        // Remove popup after animation
+        setTimeout(() => {
+          popup.remove();
+        }, 300);
+      });
+    }
+  }
+
   // Using the document$ observable is particularly important if you are using instant loading since
   // it will not result in a page refresh in the browser
   // See `How to integrate with third-party JavaScript libraries` guideline:
   // https://squidfunk.github.io/mkdocs-material/customization/?h=javascript#additional-javascript
   document$.subscribe(function () {
     setCopyText();
+    showSponsorPopup();
   });
--- docs/index.md
+++ docs/index.md
@@ -1,3 +1,12 @@
+<div style="display: flex; justify-content: center;">
+    <img src="assets/logo.svg" alt="MQPy Logo" style="width: 200px; height: 200px;">
+</div>
+
+!!! sponsor "Support MQPy Development"
+    MQPy is a free and open-source project that needs your support to continue development!
+    
+    [💛 Become a Sponsor](https://github.com/sponsors/Joaopeuko){ .md-button .md-button--primary }
+
 # MQPy
 
 MQPy is a Python library designed to simplify the process of creating Expert Advisors for MetaTrader 5. While developing directly in MQL5 can be complex, MQPy provides a more streamlined experience using Python.
