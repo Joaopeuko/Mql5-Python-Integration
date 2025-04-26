9b9ccbf0b470749aeb4bbef2b64841ad96ff40b3
Joao Paulo Euko <joaopeuko@gmail.com>
2025-04-26 16:29:33 +0000

    log in the terminal

--- tests/integration/test_mt5_connection.py
+++ tests/integration/test_mt5_connection.py
@@ -5,14 +5,20 @@ This module tests the ability to establish a connection with MT5 platform.
 
 import logging
 import os
+import sys
 import time
 
 import MetaTrader5 as mt5  # noqa: N813
 
-# Configure logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
 
 logger.info("Testing MT5 initialization...")
 
