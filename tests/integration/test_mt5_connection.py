c9bf73a326785b593e72c59ace06e2ce19c2cce9
Joao Paulo Euko <joaopeuko@gmail.com>
2025-04-26 16:34:11 +0000

    pre-commit

--- tests/integration/test_mt5_connection.py
+++ tests/integration/test_mt5_connection.py
@@ -16,7 +16,7 @@ logger.setLevel(logging.INFO)
 console_handler = logging.StreamHandler(sys.stdout)
 console_handler.setLevel(logging.INFO)
 
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 
